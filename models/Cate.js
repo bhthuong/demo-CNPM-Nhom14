5a92532446fe6193f9530b645d8b97e04739247a
mja37 <83824818+mja37@users.noreply.github.com>
2022-01-10 23:35:15 +0700

    deleted file

--- models/Cate.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var cates = new Schema({   
-    namecate: {type: String, required: false}
-}); 
-
-module.exports = mongoose.model('cates', cates);
