a2d3d00ec48fb0e0d574bdc498e653dd0bfc522c
mja37 <83824818+mja37@users.noreply.github.com>
2022-01-10 23:37:32 +0700

    update file

---
+++ models/Cate.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var cates = new Schema({   
+    namecate: {type: String, required: false}
+}); 
+
+module.exports = mongoose.model('cates', cates);
